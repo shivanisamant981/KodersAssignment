1caefe66e9b9629f967f658c12d1d59d9127defa
shivani <shivanisamant981@gmail.com>
2026-01-14 18:59:20 +0530

    created backend structure done api testing

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const app=express();
+require('dotenv').config();
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("hello ");
+})
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("running...")
+})

28c9a51405e048fe989423d07823cfe777ee104c
shivani <shivanisamant981@gmail.com>
2026-01-14 19:12:51 +0530

    inside config db.js done

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 const express=require("express");
+const connectDB = require("./config/db");
+
 const app=express();
 require('dotenv').config();
 app.use(express.json())
+
 
 app.get("/",(req,res)=>{
     res.send("hello ");
 })
 
+connectDB();
+
 
 
 
 app.listen(process.env.PORT,()=>{
-    console.log("running...")
+    console.log(`running...${process.env.PORT}`)
 })
